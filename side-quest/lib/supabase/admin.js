891d2f0371a8bb808473cad16333d1b6802d0268
liam-aga <143634606+liam-aga@users.noreply.github.com>
2025-10-26 23:03:39 -0500

    Comments for clarity

--- side-quest/lib/supabase/admin.js
+++ side-quest/lib/supabase/admin.js
@@ -2,12 +2,12 @@ import { createClient } from '@supabase/supabase-js'
 
 export function createAdminClient() {
   return createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.SUPABASE_SERVICE_ROLE_KEY,
+    process.env.NEXT_PUBLIC_SUPABASE_URL,  // supabase project URL
+    process.env.SUPABASE_SERVICE_ROLE_KEY, //  Admin role key
     {
       auth: {
-        autoRefreshToken: false,
-        persistSession: false
+        autoRefreshToken: false,  // don't refresh token automatically
+        persistSession: false  // do not persist session in storage
       }
     }
   )
