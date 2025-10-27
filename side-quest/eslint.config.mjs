dc9b47cc05491f6243c714bcaebf40b287a30eb4
liam-aga <143634606+liam-aga@users.noreply.github.com>
2025-10-26 23:54:59 -0500

    Inline comments added

--- side-quest/eslint.config.mjs
+++ side-quest/eslint.config.mjs
@@ -2,17 +2,20 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
+// Resolve __dirname 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+// FlatCompat allows extending configs in ESM
 const compat = new FlatCompat({
 	baseDirectory: __dirname,
 });
 
+// ESLint configuration array
 const eslintConfig = [
 	...compat.extends("next/core-web-vitals"),
 	{
-		ignores: [
+		ignores: [  // File folders to ignore
 			"node_modules/**",
 			".next/**",
 			"out/**",
